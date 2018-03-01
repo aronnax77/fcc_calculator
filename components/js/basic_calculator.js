f3e7b6d463be36b897080bc07e69a224d1d45f45
Richard Myatt <lydgate77@hotmail.co.uk>
2018-03-01 15:31:19 +0000

    basic calculator now completed and working

---
+++ components/js/basic_calculator.js
@@ -0,0 +1,57 @@
+/* Author: Richard Myatt
+   Date: 28 February 2018
+
+   A simple calculator using Vue.js - testing proof of concept
+*/
+
+var keyDesignations = ["(", ")", "AC", "CE",
+                       "7", "8", "9", "/",
+                       "4", "5", "6", "*",
+                       "1", "2", "3", "-",
+                       "0", ".", "=", "+"];
+
+new Vue({
+  el: "#app",
+  data: {
+    clr: true,
+    str: "",
+    res: "",
+    keys: keyDesignations
+  },
+  methods: {
+    handleKey: function(i) {      /* method to handle key press */
+      if(this.keys[i] === "AC") {
+        this.str = "";
+        this.res = "";
+        this.clr = true;
+      } else if(this.keys[i] === "CE") {
+        if(this.clr === true) {
+          this.str = this.str.slice(0, -1);
+        }
+      } else if(this.keys[i] === "=") {
+        try {
+          var result = Math.round(eval(this.str) * 100) / 100;
+          if(result < 99999999999) {
+            this.res = result;
+          } else {
+            this.res = "Error";
+            this.clr = false;
+          }
+        }
+        catch(err) {
+          this.res = "Error";
+        }
+        this.clr = false;
+      } else {
+        if(this.clr === true) {
+          if(this.str.length < 19) {
+            this.str += this.keys[i];
+          } else {
+            this.res = "Error";
+            this.clr = false;
+          }
+        }
+      }
+    }
+  }
+});
